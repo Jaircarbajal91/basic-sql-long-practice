5da77da82ae78136df99b632f3e294fa4d70834b
Brian8771 <brian8771@gmail.com>
2022-06-15 17:21:12 -0500

    3rd phase done

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,1 +1,47 @@
 -- Your code here
+-- 1.
+INSERT INTO customers  (name, phone) VALUES ('Rachel', 1111111111);
+
+SELECT points FROM customers WHERE name = 'Rachel';
+
+UPDATE customers SET points = 6 WHERE name = 'Rachel';
+
+INSERT INTO coffee_orders (is_redeemed) VALUES (0);
+
+INSERT INTO customers (name, email, phone) VALUES ('Monica', 'monica@friends.show', 2222222222),
+('Phoebe', 'phoebe@friends.show', 3333333333);
+
+UPDATE customers SET points = 8 WHERE name = 'Phoebe';
+
+INSERT INTO coffee_orders (is_redeemed) VALUES (0), (0), (0);
+
+UPDATE customers SET points = 10 WHERE name = 'Rachel';
+
+UPDATE customers SET points = 9 WHERE name = 'Monica';
+
+INSERT INTO coffee_orders (is_redeemed) VALUES (0), (0), (0), (0), (0), (0), (0), (0);
+
+SELECT points FROM customers WHERE name = 'Monica';
+
+SELECT points FROM customers WHERE name = 'Rachel';
+
+UPDATE customers SET points = 0 WHERE name = 'Rachel';
+
+INSERT INTO coffee_orders (is_redeemed) VALUES (1);
+
+INSERT INTO customers (name, email) VALUES ('Joey', 'joey@friends.show'),
+('Chandler', 'chandler@friends.show'), ('Ross', 'ross@friends.show');
+
+UPDATE customers SET points = 11 WHERE name = 'Ross';
+
+INSERT INTO coffee_orders (is_redeemed) VALUES (0), (0), (0), (0), (0), (0);
+
+UPDATE customers SET points = 12 WHERE name = 'Monica';
+
+INSERT INTO coffee_orders (is_redeemed) VALUES (0), (0), (0);
+
+UPDATE customers SET points = 9 WHERE name = 'Phoebe';
+
+INSERT INTO coffee_orders (is_redeemed) VALUES (0);
+
+DELETE FROM coffee_orders WHERE id = 18;
